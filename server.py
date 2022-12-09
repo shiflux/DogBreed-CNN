41ef78a8256ba7e1b6bc34032d7d045af29d0d0a
Artem Piazzi Tsigler <a.piazzi@frigel.com>
2022-12-09 15:29:49 +0100

    Added development mode to server.py

--- server.py
+++ server.py
@@ -1,20 +1,8 @@
 from fastapi import FastAPI, File
 from cnn.dog_breeder_model import DogBreeder
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 dogbreed = FastAPI()
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
-dogbreed.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 dog_breed_model = DogBreeder()
 
@@ -42,3 +30,20 @@ async def predict_dog(file: bytes=File()):
         return {
             'error': 'invalid_image'
         }
+        
+if __name__ == "__main__":
+    
+    from fastapi.middleware.cors import CORSMiddleware
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
+    dogbreed.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    uvicorn.run("server:dogbreed", host="127.0.0.1", port=8000, reload=True)
